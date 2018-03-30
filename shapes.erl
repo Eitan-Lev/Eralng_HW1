03cdc2d6b32bcb7b8ebe4b8261396c747f78586f
Eitan <eitanlev@gmail.com>
2018-03-30 12:59:12 +0300

    Excersize 1 begin, warning appear

---
+++ shapes.erl
@@ -0,0 +1,12 @@
+-module(shapes).
+-export([shapesArea/1]).
+-export([squaresArea/1]).
+-export([trianglesArea/1]).
+-export([shapesFilter/1]).
+-export([shapesFilter2/1]).
+
+shapesArea(Data) -> io:format("Hello world!~n").
+squaresArea(Data) -> io:format("Hello world!~n").
+trianglesArea(Data) -> io:format("Hello world!~n").
+shapesFilter(Data) -> io:format("Hello world!~n").
+shapesFilter2(Data) -> io:format("Hello world!~n").
