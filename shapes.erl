ef94955ceb2de8341f828e5eb450bbdc7f5e56fa
Eitan <eitanlev@gmail.com>
2018-03-31 19:41:18 +0300

    Excersize 1 done

--- shapes.erl
+++ shapes.erl
@@ -11,29 +11,17 @@ squaresArea(Data) -> _=checkValidData(Data),calculateArea((shapesFilter2(square)
 
 trianglesArea(Data) -> _=checkValidData(Data),calculateArea((shapesFilter(triangle))(Data)).
 
-checkValidData(Data) ->
-  case Data of
-    {shapes,[]} -> {shapes,[]};
-    {shapes,[{Shape,{Param1,Param2,Param3}}|T]} when
-      ((((Shape == triangle) or (Shape == rectangle)) and (Param1 == dim))
-      or ((Shape == ellipse) and (Param1 == radius))
-      and ((Param2 > 0) and (Param3 > 0))) -> checkValidData({shapes,T})
-  end.
-calculateArea(Data) -> calcArea(Data, 0).
-calcArea(Data, Sum) ->
-  case Data of
-    {_, []} -> Sum;
-    {_,[{triangle,{_dim,Base,Height}}|T]} -> calcArea({shapes,T},(Sum + ((Base+Height)/2)));
-    {_,[{rectangle,{_dim,Width,Height}}|T]} -> calcArea({shapes,T},(Sum + (Width*Height)));
-    {_,[{ellipse,{_redius,Radius1,Radius2}}|T]} -> calcArea({shapes,T},(Sum + (math:pi())*Radius1*Radius2))
-  end.
-
 shapesFilter(X) -> case X of
   rectangle -> ShapeType = rectangle;
   triangle -> ShapeType = triangle;
   ellipse -> ShapeType = ellipse
 end, fun(N) -> (filterList(N, ShapeType)) end.
 
+shapesFilter2(X) -> case X of
+  square -> fun(N) -> (filterList(N, square)) end;
+  circle -> fun(N) -> (filterList(N, circle)) end;
+  _ -> shapesFilter(X)
+end.
 
 filterSquareCircleList(List,ShapeType) ->
   [{Y,{_Param1,Param2,Param3}} || {Y,{_Param1,Param2,Param3}} <- List,
@@ -47,8 +35,20 @@ filterList({Atom,List}, ShapeType) -> _=checkValidData({Atom,List}),
     {Y,{_Param1,_Param2,_Param3}} <- List, Y == ShapeType]}
   end.
 
-shapesFilter2(X) -> case X of
-  square -> fun(N) -> (filterList(N, square)) end;
-  circle -> fun(N) -> (filterList(N, circle)) end;
-  _ -> shapesFilter(X)
-end.
+checkValidData(Data) ->
+  case Data of
+    {shapes,[]} -> {shapes,[]};
+    {shapes,[{Shape,{Param1,Param2,Param3}}|T]} when
+      ((((Shape == triangle) or (Shape == rectangle)) and (Param1 == dim))
+      or ((Shape == ellipse) and (Param1 == radius))
+      and ((Param2 > 0) and (Param3 > 0))) -> checkValidData({shapes,T})
+  end.
+
+calculateArea(Data) -> calcArea(Data, 0).
+calcArea(Data, Sum) ->
+  case Data of
+    {_, []} -> Sum;
+    {_,[{triangle,{_dim,Base,Height}}|T]} -> calcArea({shapes,T},(Sum + ((Base*Height)/2)));
+    {_,[{rectangle,{_dim,Width,Height}}|T]} -> calcArea({shapes,T},(Sum + (Width*Height)));
+    {_,[{ellipse,{_redius,Radius1,Radius2}}|T]} -> calcArea({shapes,T},(Sum + (math:pi())*Radius1*Radius2))
+  end.
