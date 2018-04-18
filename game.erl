2ca0ffd7f1658e4eb9bcafd585d8b05566840b8c
Amit <amitweiss90@gmail.com>
2018-04-18 16:22:24 +0300

    add stuff to game.q3

--- game.erl
+++ game.erl
@@ -14,14 +14,15 @@ nextMove(N) when N > 0 ->
     false -> false;
     true when (N =< 2) -> {true,N};
     _ ->
-      case {canWin(N-1),canWin(N-2)} of %Player 2 possible actions
-        {true,true} -> false;%Player 2 wins either way
-        {true,false} -> {true,2};%Player 2 wins only if Player 1 draws 1
-        {false,_} -> {true,1}%Player 1 can win if he draws 1
+      case {canWin(N-1),canWin(N-2)} of       %Player 2 possible actions
+        {true,true} -> false;                 %Player 2 wins either way
+        {true,false} -> {true,2};             %Player 2 wins only if Player 1 draws 1
+        {false,_} -> {true,1}                 %Player 1 can win if he draws 1
       end
   end.
 
 explanation() -> {'We have an opponent in the game, so we must consider every
-  option of his moves. We get a tree type of recursion. Tail recursion
-  requires saving the memoryy of recursive calls. It is difficult to make
-  the tree recursion behave as a tail recursion'}.
+  option of his moves.When we do that - we get a tree type of recursion.
+  Tail recursion requires saving the memory of recursive calls, and when we
+  try to do the same for tree recursion it requires a lot of memory.
+  It is difficult to make the tree recursion behave as a tail recursion'}.
